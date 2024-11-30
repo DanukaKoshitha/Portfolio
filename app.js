a65ecdc4cb7570c9ba8e887b53646b7d6b58e9b8
Danuka koshitha <danukakoshitha2004@gmail.com>
2024-12-01 03:37:15 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let tablinks = document.getElementsByClassName("links");
+let tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+
+
+
+
+
+
+
+
+
